785cd3579bc39c5ba1bf9420fd8b01b1289a7bd7
haydenDFO <162184368+haydenDFO@users.noreply.github.com>
2024-06-12 10:05:12 -0700

    Update global.js

--- public/restoration/global.js
+++ public/restoration/global.js
@@ -1,5 +1,5 @@
 const dataset = {
-  path: './data/SalmonHabitatRestorationProjects_DataPortal_June_FinalFields_20240612(in).csv',
+  path: './data/SalmonHabitatRestorationProjects_DataPortal_June_FinalFields_20240612.csv',
   headers: {
     'site_species_id': 'site_species_id',
     'project_name': 'project_name',
